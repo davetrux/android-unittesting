295d1d0b4c7e764bf80ff32a112f1cf6670b2201
Dave Truxall <david.truxall@hp.com>
2014-11-23 10:33:30 -0500

    Successful unit test w/Robolectric

--- TestableApp/app/src/androidTest/java/com/hp/es/usps/testable/testableapp/ApplicationTest.java
+++ TestableApp/app/src/androidTest/java/com/hp/es/usps/testable/testableapp/ApplicationTest.java
@@ -1,13 +1,30 @@
 package com.hp.es.usps.testable.testableapp;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.app.Activity;
+import android.widget.TextView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+@RunWith(RobolectricTestRunner.class)
+public class ApplicationTest {
+    @Test
+    public void testInstantiation() {
+
+        Activity activity = new Activity();
+
+        TextView tv = new TextView(activity);
+        tv.setText("e84");
+
+        assertEquals("e84", tv.getText());
+
+
     }
 }
